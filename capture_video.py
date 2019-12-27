d6b3200dd31f12cb3173339b4f500d2692a06704
jamjamjam <50306919+jamjamjam888@users.noreply.github.com>
2019-12-27 16:46:00 +0900

    Add files via upload

---
+++ capture_video.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+#ラズパイカメラをマウントしてusbカメラにするコマンド
+#sudo modprobe bcm2835-v4l2
+
+#usbカメラで動かすことに成功
+
+import time
+import math
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while (True):
+    #VideoCaptureから1フレーム読み込む
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+#加工なし画像を表示する
+    cv2.imshow('Raw Frame', frame)
+    
+    #キー入力を1ms待って、k が27（ESC）だったらBreakする
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+
+# キャプチャをリリースして、ウィンドウをすべて閉じる
+cap.release()
+cv2.destroyAllWindows()
+print("終了")
+################################################
